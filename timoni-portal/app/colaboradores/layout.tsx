ce421e4d7de784ae1d9be5795ec7e31d9f654a15
mcrodini-Hub <mcrodini@gmail.com>
2026-08-08 10:57:54 -0300

    Separa formulario e leitura do Espaco Equipe

--- timoni-portal/app/colaboradores/layout.tsx
+++ timoni-portal/app/colaboradores/layout.tsx
@@ -51,8 +51,7 @@ export default async function ColaboradoresLayout({
 
       <main className="mx-auto max-w-7xl px-4 py-5 sm:px-6 sm:py-7">
         {children}
-        <EspacoEquipeForm members={TEAM_MEMBERS} />
-        {isGestao && <EspacoEquipeInbox />}
+        {isGestao ? <EspacoEquipeInbox /> : <EspacoEquipeForm members={TEAM_MEMBERS} />}
       </main>
     </div>
   );
